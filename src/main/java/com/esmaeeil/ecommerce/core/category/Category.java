617be1250ecc116589b0e686e1311863e63f18ef
esmaeeil enani <e.enani@futurework.com.sa>
2025-02-19 09:56:18 +0300

    create database tables for product and category

--- src/main/java/com/esmaeeil/ecommerce/core/category/Category.java
+++ src/main/java/com/esmaeeil/ecommerce/core/category/Category.java
@@ -1,4 +1,19 @@
 package com.esmaeeil.ecommerce.core.category;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
 public class Category {
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+
 }
--- src/main/java/com/esmaeeil/ecommerce/core/product/Product.java
+++ src/main/java/com/esmaeeil/ecommerce/core/product/Product.java
@@ -1,4 +1,38 @@
 package com.esmaeeil.ecommerce.core.product;
 
+import com.esmaeeil.ecommerce.core.category.Category;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Setter
+@Getter
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String name;
+
+    @Column
+    private String summary;
+
+    @Column
+    private String description;
+
+    @Column
+    private BigDecimal price;
+
+
+    @Column(name = "fk_category_id")
+    private Long categoryId;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "fk_category_id", insertable = false, updatable = false)
+    private Category category;
+
 }
